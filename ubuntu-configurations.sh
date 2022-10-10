a35ac6ef4aa1ad65e3201f97e4fdf36d24fc4d99
Genry <gleyvaglez@gmail.com>
2022-10-09 23:41:51 -0300

    full non interactive ubuntu config

--- ubuntu-configurations.sh
+++ ubuntu-configurations.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+echo "Configuring VARs for non interactive dialogs"
+export NEEDRESTART_MODE=a
+export DEBIAN_FRONTEND=noninteractive
+
 echo "Running apt update"
 apt update && apt dist-upgrade -y
 
 echo "Install MySQL Server 5.7.29"
 apt install -y libtinfo5 libmecab2
 
-mkdir mysql && cd mysql
-
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-client_5.7.29-1ubuntu18.04_amd64.deb
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-common_5.7.29-1ubuntu18.04_amd64.deb
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-community-client_5.7.29-1ubuntu18.04_amd64.deb
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-community-server_5.7.29-1ubuntu18.04_amd64.deb
-wget https://downloads.mysql.com/archives/get/p/23/file/mysql-server_5.7.29-1ubuntu18.04_amd64.deb
-wget https://downloads.mysql.com/archives/get/p/23/file/mysql-server_5.7.29-1ubuntu18.04_amd64.deb-bundle.tar
-wget https://downloads.mysql.com/archives/get/p/23/file/mysql-testsuite_5.7.29-1ubuntu18.04_amd64.deb
+# wget https://downloads.mysql.com/archives/get/p/23/file/mysql-server_5.7.29-1ubuntu18.04_amd64.deb
+# wget https://downloads.mysql.com/archives/get/p/23/file/mysql-testsuite_5.7.29-1ubuntu18.04_amd64.deb
 
 dpkg -i ./mysql-common_5.7.29-1ubuntu18.04_amd64.deb
 dpkg -i ./mysql-community-client_5.7.29-1ubuntu18.04_amd64.deb
 dpkg -i ./mysql-client_5.7.29-1ubuntu18.04_amd64.deb
+debconf-set-selections <<< 'mysql-community-server mysql-community-server/root-pass password root'
+debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password root'
 dpkg -i ./mysql-community-server_5.7.29-1ubuntu18.04_amd64.deb
-dpkg -i ./mysql-server_5.7.29-1ubuntu18.04_amd64.deb
+# dpkg -i ./mysql-server_5.7.29-1ubuntu18.04_amd64.deb
 
 echo "Install Apache Server 2.0"
-DEBIAN_FRONTEND=noninteractive apt install -y apache2
+apt install -y apache2
 
 a2dismod mpm_event
 a2enmod mpm_prefork
@@ -34,10 +37,14 @@ echo "Install PHP 7.4 & PHP Extensions"
 apt install -y lsb-release ca-certificates apt-transport-https software-properties-common
 add-apt-repository -y ppa:ondrej/php
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y php7.4
-DEBIAN_FRONTEND=noninteractive apt install -y php7.4-mysql php7.4-xml php7.4-curl php7.4-mbstring php7.4-gd
+apt install -y php7.4
+apt install -y php7.4-mysql php7.4-xml php7.4-curl php7.4-mbstring php7.4-gd
 
 echo "Install PHP Composer"
-DEBIAN_FRONTEND=noninteractive apt install -y php7.4-cli
+apt install -y php7.4-cli
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+echo "Doing Other Tasks"
+mysql -u root -proot -e "create database db_drupal; GRANT ALL PRIVILEGES ON db_drupal.* TO user_drupal@localhost IDENTIFIED BY 'user_drupal_pass'"
+echo -e "<?php phpinfo(); ?>" >> /var/www/html/phpinfo.php
