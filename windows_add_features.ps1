284f8d45238eb50e44e738081c36a813e045578f
Genry <gleyvaglez@gmail.com>
2022-10-14 13:52:25 -0300

    ps aio

--- windows_add_features.ps1
+++ windows_add_features.ps1
@@ -1,1 +1,9 @@
-.\install_iis.ps1
+# Install IIS
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+
+# Install WebPI & PHP
+$url = "https://download.microsoft.com/download/8/4/9/849DBCF2-DFD9-49F5-9A19-9AEE5B29341A/WebPlatformInstaller_x64_en-US.msi"
+$output = ".\WebPlatformInstaller_amd64_en-US.msi"
+Invoke-WebRequest -Uri $url -OutFile $output
+.\WebPlatformInstaller_amd64_en-US.msi /quiet
+WebPICMD.exe /Install /Products:"PHP 7.4.13 (x64)" /AcceptEULA
